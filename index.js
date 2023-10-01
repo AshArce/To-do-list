17e72b00703e7ff4114939d461ba85b196027263
AshArce <arcebuchejohnashley@gmail.com>
2023-10-02 02:04:18 +0800

    its working not but to do list tasks no edit and delete

--- index.js
+++ index.js
@@ -37,20 +37,10 @@ window.addEventListener('load', () => {
     const task_actions_el = document.createElement('div');
     task_actions_el.classList.add('actions');
 
-    const task_edit_el = document.createElement('button');
-    task_edit_el.classList.add('edit');
-    task_edit_el.innerHTML = 'Edit';
-
-    const task_delete_el = document.createElement('button');
-    task_delete_el.classList.add('delete');
-    task_delete_el.innerHTML = 'Delete';
-
     const task_complete_el = document.createElement('button');
     task_complete_el.classList.add('complete');
     task_complete_el.innerHTML = 'Complete';
 
-    task_actions_el.appendChild(task_edit_el);
-    task_actions_el.appendChild(task_delete_el);
     task_actions_el.appendChild(task_complete_el);
 
     task_el.appendChild(task_actions_el);
@@ -59,30 +49,59 @@ window.addEventListener('load', () => {
 
     input.value = '';
 
-    task_edit_el.addEventListener('click', () => {
-      if (task_input_el.readOnly) {
-        task_input_el.removeAttribute('readonly');
-        task_input_el.focus();
-        task_edit_el.innerText = 'Save';
-      } else {
-        task_input_el.setAttribute('readonly', 'readonly');
-        task_edit_el.innerText = 'Edit';
-      }
-    });
+    // Add event listener to the ".complete" button.
+    task_complete_el.addEventListener('click', () => {
+      // Move the task to the completed list.
+      task_el.classList.add('completed');
 
-    task_delete_el.addEventListener('click', () => {
+      // Remove the task from the to-do list.
       list_el.removeChild(task_el);
+
+      // Add the task to the completed tasks list.
+      completedTasksDiv.appendChild(task_el);
     });
   });
 
-  // Add an event listener to the ".delete" button.
-  tasksDiv.querySelectorAll('.delete').forEach((button) => {
-    button.addEventListener('click', () => {
-      // Get the task element that the button belongs to.
-      const taskEl = button.closest('.task');
+  // Display the completed task list.
+  displayCompletedTaskList();
 
-      // Remove the task from the list.
-      list_el.removeChild(taskEl);
+  // Function to display the completed task list.
+  function displayCompletedTaskList() {
+    completedTasksDiv.innerHTML = '';
+
+    // Get all of the completed tasks.
+    const completedTasks = document.querySelectorAll('.task.completed');
+
+    // Iterate over the completed tasks and add them to the completed tasks list.
+    completedTasks.forEach((task) => {
+      const taskEl = document.createElement('div');
+      taskEl.classList.add('task');
+
+      const taskContentEl = document.createElement('div');
+      taskContentEl.classList.add('content');
+
+      taskEl.appendChild(taskContentEl);
+
+      const taskInputEl = document.createElement('input');
+      taskInputEl.classList.add('text');
+      taskInputEl.type = 'text';
+      taskInputEl.value = task.querySelector('.text').value;
+      taskInputEl.setAttribute('readonly', 'readonly');
+
+      taskContentEl.appendChild(taskInputEl);
+
+      const taskActionsEl = document.createElement('div');
+      taskActionsEl.classList.add('actions');
+
+      const taskDeleteEl = document.createElement('button');
+      taskDeleteEl.classList.add('delete');
+      taskDeleteEl.innerHTML = 'Delete';
+
+      taskActionsEl.appendChild(taskDeleteEl);
+
+      taskEl.appendChild(taskActionsEl);
+
+      completedTasksDiv.appendChild(taskEl);
     });
-  });
+  }
 });
